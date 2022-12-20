e10e269e4e7f6dbc5d542be5b8924a58329ae67c
metacoder87 <gary.miller@gmx.com>
2022-12-20 17:50:49 -0500

    Adds user table

--- db/migrate/20221220202521_create_users.rb
+++ db/migrate/20221220202521_create_users.rb
@@ -1,12 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :session_token
-      t.string :password_digest
-      t.integer :cheer_count
+      t.string :username, null: false
+      t.string :session_token, null: false
+      t.string :password_digest, null: false
+      t.integer :cheer_count, null: false
 
       t.timestamps
     end
+
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
