60ab8fe2409dd289d50d3414d7dc4f6cbeab69a9
metacoder87 <gary.miller@gmx.com>
2022-12-23 21:39:37 -0500

    Updates user class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,10 +34,6 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def user_params
