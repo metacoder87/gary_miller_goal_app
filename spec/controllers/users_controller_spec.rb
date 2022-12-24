a5fca43e282847324276be8c984e768684fa8ef1
metacoder87 <gary.miller@gmx.com>
2022-12-23 21:58:05 -0500

    Updates user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -32,17 +32,17 @@ RSpec.describe UsersController, type: :controller do
       password: "password")
   end
 
-  describe "GET #index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
-  end
-
   describe "GET #show" do
     it "renders the show template" do
       get :show, params: { id: user.id }
       expect(response).to render_template(:show)
+    end
+  end
+
+  describe "GET #index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
     end
   end
 
