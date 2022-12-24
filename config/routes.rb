32c4051b6fa87e201adda028f70fc051976d6166
metacoder87 <gary.miller@gmx.com>
2022-12-23 19:56:46 -0500

    Updates routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:index, :show, :new, :create]
+  resources :session, only: [:new, :create, :destroy]
+  root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
