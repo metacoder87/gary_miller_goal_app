2a20699da4934e6cf5aec111394c5009dfcde032
metacoder87 <gary.miller@gmx.com>
2022-12-27 16:21:01 -0500

    Adds goals resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :new, :create]
   resources :session, only: [:new, :create, :destroy]
   root to: 'users#index'
+
+  resources :goals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
