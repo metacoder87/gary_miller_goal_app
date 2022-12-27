1dc950dbc49520064a0a1e4369a9d82263aa18e0
metacoder87 <gary.miller@gmx.com>
2022-12-27 16:38:21 -0500

    Updates goal model class

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,7 @@
 class Goal < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 6 }
+
+    belongs_to :author,
+        class_name: :User,
+        foreign_key: :user_id
 end
