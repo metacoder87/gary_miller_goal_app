924e7c25814cdbc82df984d8c6eea7263c454b47
metacoder87 <gary.miller@gmx.com>
2022-12-28 12:02:20 -0500

    Updates goal bot

--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -1,9 +1,6 @@
 FactoryBot.define do
   factory :goal do
-    title { "MyString" }
-    private { false }
-    details { "MyText" }
-    completed { false }
-    user_id { 1 }
+    title { Faker::Lorem.words(3).join(" ") }
+    details { Faker::Lorem.words(5).join(" ") }
   end
 end
