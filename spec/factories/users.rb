28d3d51ab23b3eb3fa01661d6509d1a7352f6b4a
metacoder87 <gary.miller@gmx.com>
2022-12-28 12:02:30 -0500

    Updates User bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    session_token { "MyString" }
-    password_digest { "MyString" }
-    cheer_count { 1 }
+    username { Faker::Internet.user_name }
+    password { "password" }
+
+    factory :user_hw do
+      username { "hello_world" }
+    end
+
+    factory :user_foo do
+      username { "foo_bar" }
+    end
   end
 end
