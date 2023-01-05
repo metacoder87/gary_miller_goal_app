7f5a3cb1347396f0cad46d448139cbddcc70ce3a
metacoder87 <gary.miller@gmx.com>
2023-01-04 20:39:16 -0500

    Updates goal model class

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,16 @@
 class Goal < ApplicationRecord
+    include Commentable
+
     validates :title, presence: true, length: { minimum: 6 }
 
     belongs_to :author,
         class_name: :User,
         foreign_key: :user_id
+
+    has_many :cheers
+
+    def cheered_by?(user)
+        cheers.exists?(giver_id: user.id)
+    end
+
 end
