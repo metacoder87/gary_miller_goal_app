c020df08e72d27b297b0bb9a4bfc740cfa490d49
metacoder87 <gary.miller@gmx.com>
2023-01-20 06:03:32 -0500

    Adds new user route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :cheers, only: [:create]
   end
   resources :users, only: [:index, :show, :new, :create]
+  post "/users/new", to: "users#create"
   resources :session, only: [:new, :create, :destroy]
   root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
