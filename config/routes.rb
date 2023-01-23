908f2336a052723a5f502617436ba73b104e059f
metacoder87 <gary.miller@gmx.com>
2023-01-23 17:04:22 -0500

    Route redo

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:create]
-  resources :cheers, only: [:index]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:index, :show, :new, :create]
   resources :goals do
     resources :cheers, only: [:create]
   end
-  resources :users, only: [:index, :show, :new, :create]
-  post "/users/new", to: "users#create"
-  resources :session, only: [:new, :create, :destroy]
-  root to: 'users#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cheers, only: [:index]
+  resources :comments, only: [:create]
 end
